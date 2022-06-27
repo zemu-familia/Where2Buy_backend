b19e6aeaca680aa13c94044179d688f6c46d2386
djhkm123 <hakimdjhkm@gmail.com>
2022-06-28 00:37:13 +0800

    add request backend

---
+++ addRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('db.php');
+require_once('functions.php');
+
+if(true){
+  $requesterID = filter_input(INPUT_POST, 'requesterID', FILTER_SANITIZE_NUMBER_INT);
+  $requesterLat = filter_input(INPUT_POST, 'requesterLat', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+  $requesterLng = filter_input(INPUT_POST, 'requesterLng', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+  $itemName = mysqli_real_escape_string($conn, filter_input(INPUT_POST, 'itemName', FILTER_SANITIZE_STRING));
+  $areaRadius = filter_input(INPUT_POST, 'areaRadius', FILTER_SANITIZE_NUMBER_INT);
+
+
+  $query = "INSERT INTO REQUEST(requesterUserID, itemName, areaCenterRadius, areaCenterLat, areaCenterLng, requestDate) VALUES('$requesterID', '$itemName', '$areaRadius', '$requesterLat', '$requesterLng', CURRENT_DATE)";
+
+  $result = mysqli_query($conn, $query);
+  if($result){ // If query succeeds
+    statusCodeJSON($jsonArray, 600, "Request successfully sent.");
+  }else{
+    statusCodeJSON($jsonArray, 610, "Query failed. " . mysqli_error($conn));
+    $jsonArray["query"] = array("queryFull" => $query);
+  }
+}else{
+  statusCodeJSON($jsonArray, 401, "An error occurred");
+}
+echo json_encode($jsonArray);
+
+?>
