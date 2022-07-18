70f5277b349d2cc4ffb4737cd3e16b28b68c735a
djhkm123 <hakimdjhkm@gmail.com>
2022-07-18 21:39:01 +0800

    upload file for request

--- addRequest.php
+++ addRequest.php
@@ -9,20 +9,36 @@ if(true){
   $requesterLng = filter_input(INPUT_POST, 'requesterLng', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
   $itemName = ucwords(mysqli_real_escape_string($conn, filter_input(INPUT_POST, 'itemName', FILTER_SANITIZE_STRING)));
   $areaRadius = filter_input(INPUT_POST, 'areaRadius', FILTER_SANITIZE_NUMBER_INT);
+  $imageBase64 = $_POST['imageBase64'];
 
+  $imagePath = "./image/request/";
+  $fileName = "$requesterID-".date("dmyHis").".jpg";
+  $combinedPath = $imagePath.$fileName;
 
-  $query = "INSERT INTO REQUEST(requesterUserID, itemName, areaCenterRadius, areaCenterLat, areaCenterLng, requestDate) VALUES('$requesterID', '$itemName', '$areaRadius', '$requesterLat', '$requesterLng', CURRENT_TIMESTAMP)";
-
-  $result = mysqli_query($conn, $query);
-  if($result){ // If query succeeds
-    statusCodeJSON($jsonArray, 600, "Request successfully sent.");
-  }else{
-    statusCodeJSON($jsonArray, 610, "Query failed. " . mysqli_error($conn));
-    $jsonArray["query"] = array("queryFull" => $query);
+  if(!is_dir($imagePath)){
+    mkdir($imagePath, 0777, true);
   }
+
+  if(file_put_contents("$combinedPath", base64_decode($imageBase64))){
+    $query = "INSERT INTO REQUEST(requesterUserID, itemName, areaCenterRadius, areaCenterLat, areaCenterLng, requestDate, imagePath) VALUES('$requesterID', '$itemName', '$areaRadius', '$requesterLat', '$requesterLng', CURRENT_TIMESTAMP, '$combinedPath')";
+
+    $result = mysqli_query($conn, $query);
+    if($result){ // If query succeeds
+      statusCodeJSON($jsonArray, 600, "Request successfully sent.");
+    }else{
+      statusCodeJSON($jsonArray, 610, "Query failed. " . mysqli_error($conn));
+    }
+  }
+  else {
+    statusCodeJSON($jsonArray, 610, "File upload failed. " . mysqli_error($conn));
+  }
+
+  //$query = "INSERT INTO REQUEST(requesterUserID, itemName, areaCenterRadius, areaCenterLat, areaCenterLng, requestDate) VALUES('$requesterID', '$itemName', '$areaRadius', '$requesterLat', '$requesterLng', CURRENT_TIMESTAMP)";
+
 }else{
   statusCodeJSON($jsonArray, 401, "An error occurred");
 }
+//$jsonArray["query"] = array("queryFull" => $query);
 echo json_encode($jsonArray);
 
 ?>
